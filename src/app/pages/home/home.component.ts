0686d78ebcbbd5c2e702fa6fcef9838d60f70636
ddesperaate <system.x1060@gmail.com>
2023-12-06 18:56:56 +0200

    fix link 2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home',
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
   searchText: string = '';
 
-  constructor() {}
+  constructor(
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
   }
 
+  showRoute(path): void {
+    this.router.navigate(['/' + path]);
+  }
 }
